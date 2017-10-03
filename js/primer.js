a8a0635f3070470ff8bf941aa1fd106bd73b6a9c
alexandre <kalambik@gmail.com>
2017-10-03 17:25:24 +0200

    font-animation

--- js/primer.js
+++ js/primer.js
@@ -1,39 +1,39 @@
 
 
 $(document).ready(function() {
-
-
-	
-	var $target = $('body');
-	
-	$('.section').on('inview', function(el){
-		var color = $(el).attr('data-background');
-		$target.css('background-color', color );
-	});
-	/*let hauteur = $(window).height();
-	$('.section')
-	.height(hauteur)
-	.scrollie({
-		//direction : 'both',
-		scrollOffset : -50,
-		//scrollRatio : 2,
-		scrollingInView : function(elem) {
-			var Color = $(elem).attr('data-background-color');
-			$('#pagepiling').css('background-color', Color);
-
-		}
-	});*/
-
-
-
+	$(".screenshot2").css("opacity", "0");
 	$('#pagepiling').pagepiling({
-		//sectionsColor: ['#4FD0E0', '#2ebe21', '#2C3E50', '#51bec4'],
+		afterLoad: function(anchorLink,index){
+			//section 1
+			if(index === 1){
+				$(".section").css('background-color', '#4FD0EA');
+				$(".screenshot2").css("opacity", "0");
+				$(".screenshot2").animate({height: '0'}, "slow");
+			}
+            //section2
+			if(index === 2){
+				$(".section").css('background-color', '#69B982');
+				$(".bg_rect").css('opacity','1');
+				$(".screenshot2").css("opacity", "1");
+				$(".screenshot2").animate({height: '825px'}, "slow");
+			}
+			//section3
+			if(index === 3){
+				$(".section").css('background-color', '#4CB7B8');
+				$(".bg_rect").css('opacity','0');
+			}
+			//section4
+			if(index === 4){
+				$(".section").css('background-color', '#A5D370');
+				$(".bg_rect").css('opacity','0');
+			}
+		},
 		direction: 'vertical',
 		verticalCentered: true,
-		scrollingSpeed: 300,
+		scrollingSpeed: 100,
 		easing: 'swing',
-		loopBottom: false,
-		loopTop: false,
+		//loopBottom: false,
+		//loopTop: false,
 		css3: true,
 		navigation:false,
 		normalScrollElements: null,
@@ -41,12 +41,15 @@ $(document).ready(function() {
 		touchSensitivity: 5,
 		keyboardScrolling: true,
 		sectionSelector: '.section',
-		animateAnchor: false,
-
+		animateAnchor: false
 	});
 
+
+	
 	//$.fn.pagepiling.setScrollingSpeed(400);
 
 	
 
 });
+
+
