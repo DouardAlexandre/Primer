17a9dea074bbc132b46aaa1462997e196b72adab
alexandre <kalambik@gmail.com>
2017-10-05 15:48:02 +0200

    integration objects scene

--- js/primer.js
+++ js/primer.js
@@ -14,20 +14,15 @@ $(window).ready(function() {
 			if(index === 1){
 
 				//First paragraph
-				$(".firstTitle").animate({"margin-top": '1rem'}, "slow").fadeIn();
-            	$(".firstText").fadeIn();
+				$(".firstTitle").fadeIn("300").animate({"margin-top": '1rem'}, "slow");
+				$(".firstText").fadeIn("300").animate({"margin-top": '1rem'}, "slow");
             	//second paragraph
-            	//$(".secondTitle").css({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	//$(".secondText").fadeOut();
-            	 //third paragraph
-            	//$(".thirdTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	//$(".thirdText").fadeOut();
-            	//fourth paragraph
-            	//$(".fourthTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	//$(".fourthText").fadeOut();
-
+            	$(".secondTitle").animate({"margin-top": '1.5rem'}, "fast").fadeOut("500");
+            	$(".secondText").animate({"margin-top": '1.5rem'}, "fast").fadeOut("500");
+            	 
+                $(".napkin").css('opacity','1');
 		        //section color
-				$(".section").css('background-color', '#4FD0EA');
+		        $(".section").css('background-color', '#4FD0EA');
 				//background section 2
 				$(".bg_rect").animate({"margin-top": '0rem'}, "slow");
 				//scroll up from section 2
@@ -43,18 +38,21 @@ $(window).ready(function() {
             //section2......................................................
 
             if(index === 2){
-
-            	scroll_arch=1;
                 //First paragraph
-            	$(".firstTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	$(".firstText").fadeOut();
+                $(".firstTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut("500");
+                $(".firstText").animate({"margin-top": '-.5rem'}, "slow").fadeOut("500");
             	//second paragraph
-            	$(".secondTitle").css("opacity": '1');
-            	$(".secondText").css("opacity": '1');
-            	$(".secondTitle").animate({"margin-top": '-.5rem'}, "slow").fadeIn();
-            	$(".secondText").fadeIn();
+            	setTimeout(function(){ 
+            		$(".secondTitle").css("opacity", '1').fadeIn().animate({"margin-top": '1.5rem'}, "slow");
+            		$(".secondText").css("opacity", '1').fadeIn().animate({"margin-top": '1.5rem'}, "slow");
+            	}, 800);
+            	//third paragraph
+            	$(".thirdTitle").animate({"margin-top": '.5rem'}, "fast").fadeOut("500");
+            	$(".thirdText").animate({"margin-top": '.5rem'}, "fast").fadeOut("500");
+            	scroll_arch=1;
+
                 //section color
-            	$(".section").css('background-color', '#69B982');
+                $(".section").css('background-color', '#69B982');
             	//background 1--2--3
             	$(".bg_arch").animate({top: '-105rem'}, "slow");
             	$(".bg_rect").animate({"margin-top": '-65rem'}, "slow");
@@ -71,49 +69,54 @@ $(window).ready(function() {
 			if(index === 3){
 				
                 //second paragraph
-            	$(".secondTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	$(".secondText").fadeOut();
+                $(".secondTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut("500");
+                $(".secondText").animate({"margin-top": '-.5rem'}, "slow").fadeOut("500");
             	//third paragraph
-            	$(".thirdTitle").animate({"margin-top": '-.5rem'}, "slow").fadeIn();
-            	$(".thirdText").fadeIn();
+            	setTimeout(function(){ 
+            		$(".thirdTitle").css("opacity", '1').fadeIn().animate({"margin-top": '-.5rem'}, "slow");
+            		$(".thirdText").css("opacity", '1').fadeIn().animate({"margin-top": '-.5rem'}, "slow");
+            	}, 600);
                 //fourth paragraph
-            	$(".fourthTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	$(".fourthText").fadeOut();
+                $(".fourthTitle").animate({"margin-top": '.5rem'}, "fast").fadeOut("500");
+                $(".fourthText").animate({"margin-top": '.5rem'}, "fast").fadeOut("500");
 
-				$(".napkin").css('opacity','0');
-				$(".section").css('background-color', '#4CB7B8');
-				$(".bg_rect").css("margin-top",'-150rem');
-				$(".bg_square").css("margin-top",'150rem');
-				$(".bg_cloud").animate({"margin-top":'-55rem'}, "slow");
-				setTimeout(function(){ 
-					
-					$(".screen3").animate({"margin-top": '-26.2rem'}, "slow");
-					$(".screen4").animate({"margin-top": '0'}, "slow");
-				}, 700);
-			}
+                $(".napkin").css('opacity','0');
+                $(".section").css('background-color', '#4CB7B8');
+                $(".bg_rect").css("margin-top",'-150rem');
+                $(".bg_square").css("margin-top",'150rem');
+                $(".bg_cloud").animate({"margin-top":'-55rem'}, "slow");
+                setTimeout(function(){ 
+
+                	$(".screen3").animate({"margin-top": '-26.2rem'}, "slow");
+                	$(".screen4").animate({"margin-top": '0'}, "slow");
+                }, 700);
+            }
 
 			//section4......................................................
 
 			if(index === 4){
 
                 //third paragraph
-            	$(".thirdTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut();
-            	$(".thirdText").fadeOut();
+                $(".thirdTitle").animate({"margin-top": '-.5rem'}, "slow").fadeOut("500");
+                $(".thirdText").animate({"margin-top": '-.5rem'}, "slow").fadeOut("500");
             	//fourth paragraph
-            	$(".fourthTitle").animate({"margin-top": '-.5rem'}, "slow").fadeIn();
-            	$(".fourthText").fadeIn();
-				$(".section").css('background-color', '#A5D370');
-				$(".bg_cloud").css('margin-top','-150rem');
-				$(".bg_square").animate({"margin-top": '-70rem'}, "slow");
-				setTimeout(function(){ 
-					$(".screen4").animate({"margin-top": '-26.2rem'}, "slow");
-				}, 700);
-			}
-		},
-		direction: 'vertical',
-		verticalCentered: true,
-		scrollingSpeed: 100,
-		easing: 'swing',
+            	setTimeout(function(){ 
+            		$(".fourthTitle").css("opacity", '1').fadeIn().animate({"margin-top": '-.5rem'}, "slow");
+            		$(".fourthText").css("opacity", '1').fadeIn().animate({"margin-top": '-.5rem'}, "slow");
+            	}, 600);
+
+            	$(".section").css('background-color', '#A5D370');
+            	$(".bg_cloud").css('margin-top','-150rem');
+            	$(".bg_square").animate({"margin-top": '-70rem'}, "slow");
+            	setTimeout(function(){ 
+            		$(".screen4").animate({"margin-top": '-26.2rem'}, "slow");
+            	}, 700);
+            }
+        },
+        direction: 'vertical',
+        verticalCentered: true,
+        scrollingSpeed: 100,
+        easing: 'swing',
 		//loopBottom: false,
 		//loopTop: false,
 		css3: true,
